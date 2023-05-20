1bb5d1a956f41175f4afe3cd14a18fb82d39d96e
Krunal Gupta <guptakrunal02@gmail.com>
2023-05-20 17:59:42 +0530

    initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdEaxpJ1j8PU9h8399wvdxI2Hitv058UA",
+  authDomain: "cryptoapp-fcf65.firebaseapp.com",
+  projectId: "cryptoapp-fcf65",
+  storageBucket: "cryptoapp-fcf65.appspot.com",
+  messagingSenderId: "172385950669",
+  appId: "1:172385950669:web:1f733ab8684c8792c94284",
+};
+
+export default firebaseConfig;
